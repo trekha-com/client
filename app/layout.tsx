cfefedcdc8a4e5e315e89f5b773605c40aa66267
Gabriel Halus <gabrielhalus@gmail.com>
2024-05-22 19:03:31 +0200

    📦 add GeistSans font family

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
+import '@/app/globals.css';
+import { cn } from '@/lib/utils';
+import { GeistSans } from 'geist/font/sans';
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en' className='h-full'>
-      <body className='h-full'>{children}</body>
+      <body className={cn(GeistSans.variable, 'font-sans h-full')}>{children}</body>
     </html>
   );
 }
