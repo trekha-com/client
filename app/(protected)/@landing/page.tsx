5ec384df78ff88d95246eb3bad32d14a15e5695d
Gabriel Halus <gabrielhalus@gmail.com>
2024-06-01 22:15:13 +0200

    📦 add links on landing page

--- app/(protected)/@landing/page.tsx
+++ app/(protected)/@landing/page.tsx
@@ -1,6 +1,16 @@
+import Link from 'next/link';
+
 export default function Landing() {
   return (
     <div>
+      <div className='flex items-center gap-3'>
+        <Link href='/login' className='hover:underline'>
+          Login
+        </Link>
+        <Link href='/register' className='hover:underline'>
+          Register
+        </Link>
+      </div>
       <h1>Landing page</h1>
     </div>
   );
