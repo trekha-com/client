6bc0cccf93e8f8849423a838eb098b5c67be78f3
Gabriel Halus <gabrielhalus@gmail.com>
2024-06-02 10:21:02 +0200

    📦 add axios interceptors

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { redirect } from 'next/navigation';
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
@@ -6,7 +7,30 @@ if (!BASE_URL) {
   throw new Error('NEXT_PUBLIC_BASE_URL is not defined. Please set the BASE_URL environment variable.');
 }
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: BASE_URL,
   headers: { 'Content-Type': 'application/json' },
 });
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    // TODO if session token, set a Bearer Authorization token
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      // Redirect to the login page if a 401 Unauthorized response is received
+      redirect('/login');
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
